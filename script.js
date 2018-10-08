645ca0328260dd5ca47b6be88cabe0164bd55aa0
Julie Semenova <js@id-media.ru>
2018-10-08 11:00:57 +0300

    ✨ methods: add and delete item

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 var card = new Vue({
-  el: "#card",
+  el: '#card',
   data: {
-    title: "Dinosaurs",
-    content: "<strong>Dinosaurs</strong> are a diverse group of animals of the clade <em>Dinosauria</em> that first appeared during the Triassic period."
+    title: 'Dinosaurs',
+    items: [{ text: 'Velociraptor' }, { text: 'Triceratops' }, { text: 'Stegosaurus' }]
+  },
+  methods: {
+    addItem: function() {
+      var input = document.getElementById('itemForm');
+
+      if (input.value !== '') {
+        this.items.push({
+          text: input.value
+        });
+        input.value = '';
+      }
+    },
+    deleteItem: function(index) {
+      this.items.splice(index, 1);
+    }
   }
-})
+});
